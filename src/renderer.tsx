9954e17d07494b49e58d36e6aa7828fec0029c32
Zach Samuels <wzsamuels@gmail.com>
2024-02-10 21:40:16 -0500

    Add dark mode support

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -32,6 +32,12 @@ import App from './App'
 import { IpcProvider } from './IpcContext';
 import './index.css';
 
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <IpcProvider>
