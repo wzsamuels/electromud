7f56db2b40deeeebc0e337d51af398c19f135580
Zach Samuels <wzsamuels@gmail.com>
2024-02-10 21:26:41 -0500

    Add new world window component

---
+++ src/components/NewWorldWindow.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NewWorldWindow = () => {
+  console.log("New window")
+  return (
+    <div className="bg-grey-400 text-blue">
+      Test
+    </div>
+  )
+}
+
+export default NewWorldWindow
