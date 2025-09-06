933cec8f12132b5149170624d286514bd6b05933
Zach Samuels <wzsamuels@gmail.com>
2025-09-06 17:25:37 -0400

    Update url regex

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -9,7 +9,7 @@ const escapeAngleBrackets = (text: string): string => {
 };
 
 const convertUrlsToLinks = (text: string): string => {
-  const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
+  const urlRegex = /(\b(https?):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
   return text.replace(urlRegex, (url) => `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`);
 };
 
